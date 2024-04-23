cd9830c219fec227139249e2d6980bbf2f6e7c82
edward <164494080+gichimuedward@users.noreply.github.com>
2024-04-23 14:38:13 +0300

    Create Integer.py

---
+++ Integer.py
@@ -0,0 +1,13 @@
+# Write a program that takes an integer as input and returns true if the input is a power of two.
+
+def is_power_of_two(n):
+  
+    return n > 0 and (n & (n - 1)) == 0
+ 
+print(is_power_of_two(5))   
+print(is_power_of_two(16))  
+print(is_power_of_two(9))  
+print(is_power_of_two(4))   
+print(is_power_of_two(20))  
+print(is_power_of_two(68))   
+print(is_power_of_two(687))
